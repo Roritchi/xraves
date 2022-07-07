84483bcb0517bebe79aa51caf052ea69f2be9018
David <git@davidschneider.xyz>
2022-07-07 20:09:31 +0200

    MONGO_URI env variable

--- utils/db.ts
+++ utils/db.ts
@@ -5,7 +5,7 @@ import {
   } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
   
 const client = new MongoClient();
-await client.connect("mongodb://127.0.0.1:27017");
+await client.connect(Deno.env.get("MONGO_URI") as string);
 
 const db = client.database("xraves");
 
